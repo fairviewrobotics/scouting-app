f7075d059658a5429cd356ff4c8386bdd5504ce9
aaditsangvikar <aadit.sangvikar@gmail.com>
2025-01-26 15:11:56 -0700

    adding /py/ to path

--- api/index.py
+++ api/index.py
@@ -3,6 +3,6 @@ from fastapi import FastAPI
 ### Create FastAPI instance with custom docs and openapi url
 app = FastAPI(docs_url="/api/docs", openapi_url="/api/openapi.json")
 
-@app.get("/api/helloFastApi")
+@app.get("/api/py/helloFastApi")
 def hello_fast_api():
     return {"message": "Hello from FastAPI"}
