4b9c937c3cd2f0eef377f7fa8a34a406b7d37d9b
aaditsangvikar <aadit.sangvikar@gmail.com>
2025-01-26 13:26:13 -0700

    still trying to get apis to work

--- api/index.py
+++ api/index.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
 ### Create FastAPI instance with custom docs and openapi url
-app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
+app = FastAPI(docs_url="/api/docs", openapi_url="/api/openapi.json")
 
 @app.get("/api/helloFastApi")
 def hello_fast_api():
