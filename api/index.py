f95cfd5848c3a4e4153ea5ef9d8c6638ac5a80d7
aaditsangvikar <aadit.sangvikar@gmail.com>
2025-01-26 17:19:52 -0700

    more api debugging

--- api/index.py
+++ api/index.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import json
 
 ### Create FastAPI instance with custom docs and openapi url
 app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
@@ -6,3 +7,8 @@ app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
 @app.get("/api/py/helloFastApi")
 def hello_fast_api():
     return {"message": "Hello from FastAPI"}
+
+@app.get("/api/py/json/match_scouting")
+async def get_match_scouting_json():
+    with open("../api/match_scouting.json", 'r') as file:
+        return json.load(file)
